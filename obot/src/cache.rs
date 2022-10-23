c0123ccd3f3b2d0d45305fece8139572373c1c02
LogWat <Nk8877wpt+Karan@gmail.com>
2022-10-24 01:02:37 +0900

    add: Owner Container

--- obot/src/cache.rs
+++ obot/src/cache.rs
@@ -1,16 +1,34 @@
 use std::{
     sync::{Arc},
+    collections::{HashMap, HashSet},
 };
 
 use serenity::{
     client::bridge::gateway::ShardManager,
+    model::{id::UserId},
     prelude::*,
 };
 
 use tokio::sync::Mutex;
 
+
+// bot操作用の構造体(shutdownとか)
 pub struct SharedManagerContainer;
 
 impl TypeMapKey for SharedManagerContainer {
     type Value = Arc<Mutex<ShardManager>>;
+}
+
+// コマンドの実行回数を記録する
+pub struct CommandCounter;
+
+impl TypeMapKey for CommandCounter {
+    type Value = Arc<Mutex<HashMap<String, u64>>>;
+}
+
+// オーナーのIDを記録する
+pub struct Owners;
+
+impl TypeMapKey for Owners {
+    type Value = Arc<Mutex<HashSet<UserId>>>;
 }
