a0baa1faa0384a53e2e6533e3e743eaa8fc389ee
LogWat <Nk8877wpt+Karan@gmail.com>
2022-11-29 01:29:47 +0900

    add: test_script download

--- tests/scrape_test.py
+++ tests/scrape_test.py
@@ -5,6 +5,7 @@ dotenv.load_dotenv()
 api_key = dotenv.get_key(dotenv.find_dotenv(), "API_SECRET")
 client_id = dotenv.get_key(dotenv.find_dotenv(), "USER_ID")
 api_base = dotenv.get_key(dotenv.find_dotenv(), "API_BASE")
+download_base = dotenv.get_key(dotenv.find_dotenv(), "DOWNLOAD_BASE")
 
 ranked_api = api_base + "/api/v2/beatmapsets/search?m=3&q=key%3D4&s=ranked&nsfw="
 loved_api = api_base + "/api/v2/beatmapsets/search?m=3&q=key%3D4&s=loved&nsfw="
@@ -35,5 +36,17 @@ token = requests.post(api_base + "/oauth/token",
         "scope":"public"
     }
 ).json()["access_token"]
+
+def download(beatmapsets_id, filename):
+    url = download_base + "{}?n=1".format(beatmapsets_id)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"}
+    res = requests.get(url, headers=headers, stream=True)
+    if res.status_code == 200:
+        with open(filename, "wb") as f:
+            for chunk in res.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+    print("Downloaded {}".format(filename))
+
 
 _ = get_beatmapsets(loved_api, token)
